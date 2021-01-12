a0b9a9fac9a385e1f2b9db10fa7b59cbafc811cc
Evghenii <egx@egx-mbp.local>
2021-01-12 12:04:40 -0800

    checkpoint

--- ch1/test1.rkt
+++ ch1/test1.rkt
@@ -2,7 +2,15 @@
 
 (require racket/struct)
 
-(struct Int (value) #:transparent)
+(struct Int (value) #:prefab)
 
-(define intval8 (Int 8))
-(printf "value: ~a\n" intval8)
+(define eight (Int 8))
+(printf "value: ~a\n" eight)
+
+(struct Prim (op args) #:prefab)
+(define neg-eight (Prim '- (list eight)))
+(define rd (Prim 'read '()))
+(define ast1.1 (Prim '+ (list rd neg-eight)))
+
+
+(printf "expr: ~a\n" ast1.1)
